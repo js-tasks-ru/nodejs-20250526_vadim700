64b61cdeb2e64e8149448d7bdbf89927ebfd4838
Вадим Бекмансуров <vadimbekmansurov@Vadims-MacBook-Pro.local>
2025-06-29 19:51:49 +0500

    added solution for 04 module 01 task

--- 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
+++ 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
@@ -1,5 +1,46 @@
-import { ArgumentsHost, ExceptionFilter } from "@nestjs/common";
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from "@nestjs/common";
+import { Response } from "express";
+import { appendFileSync } from "node:fs";
 
+@Catch()
 export class HttpErrorFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {}
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    let status: number;
+    let message: string | object;
+
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      const exceptionResponse = exception.getResponse();
+      message =
+        typeof exceptionResponse === "object"
+          ? (exceptionResponse as any).message || exceptionResponse
+          : exceptionResponse;
+    } else {
+      status = 500;
+      message = "Mock error for testing";
+    }
+
+    const errorResponse = {
+      statusCode: status,
+      message,
+      timestamp: new Date().toISOString(),
+    };
+
+    const { statusCode, timestamp } = errorResponse;
+    appendFileSync(
+      "errors.log",
+      `[${timestamp}] ${statusCode} - ${errorResponse.message}\n`,
+    );
+
+    response.status(status);
+    response.json(errorResponse);
+  }
 }
